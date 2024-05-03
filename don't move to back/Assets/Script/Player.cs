5f13617c188c48b272492a084e5d987847b4be48
shiki-01 <0110shiki@gmail.com>
2024-05-03 13:13:19 +0900

    ジャンプ実装

--- don't move to back/Assets/Script/Player.cs
+++ don't move to back/Assets/Script/Player.cs
@@ -7,14 +7,18 @@ public class Player : MonoBehaviour
 {
     [SerializeField, Header("移動速度")]
     private float _moveSpeed;
+    [SerializeField, Header("ジャンプ速度")]
+    private float _jumpSpeed;
 
     private Vector2 _inputDirection;
     private Rigidbody2D _rigid;
+    private bool _bJump;
 
     // Start is called before the first frame update
     void Start()
     {
         _rigid = GetComponent<Rigidbody2D>();
+        _bJump = false;
     }
 
     // Update is called once per frame
@@ -31,5 +35,13 @@ public class Player : MonoBehaviour
     public void _OnMove(InputAction.CallbackContext context)
     {
         _inputDirection = context.ReadValue<Vector2>();
+    }
+
+    public void _OnJump(InputAction.CallbackContext context)
+    {
+        if (!context.performed || _bJump) return;
+
+        _rigid.AddForce(Vector2.up * _jumpSpeed, ForceMode2D.Impulse);
+        _bJump = true;
     }
 }
