9369c00997b4e0aa61c4555ca2d2c284d0ce78ea
shiki-01 <0110shiki@gmail.com>
2024-05-04 15:12:42 +0900

    ふぇーとインアウト

--- don't move to back/Assets/Script/TitleManager.cs
+++ don't move to back/Assets/Script/TitleManager.cs
@@ -6,16 +6,25 @@ using UnityEngine.SceneManagement;
 
 public class TitleManager : MonoBehaviour
 {
+    private bool _bStart;
+    private Fade _fade;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        _bStart = false;
+        _fade = FindObjectOfType<Fade>();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    private void _TitleStart()
+    {
+        _bStart=true;
     }
 
     public void OnSpaceClick(InputAction.CallbackContext context)
