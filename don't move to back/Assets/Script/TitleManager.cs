8fa6aaef9be4555df4ec9dfc18871b6a70e6bb6a
shiki-01 <0110shiki@gmail.com>
2024-05-04 12:58:19 +0900

    せーぶ

---
+++ don't move to back/Assets/Script/TitleManager.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.SceneManagement;
+
+public class TitleManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnSpaceClick(InputAction.CallbackContext context)
+    {
+        if (!context.performed)
+        {
+            SceneManager.LoadScene("SampleScene");
+        }
+    }
+}
